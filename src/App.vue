ce5373dc3de2833fee9e86072289259c1219ef19
gaoxu <gaoxg@dreamdt.cn>
2023-06-28 17:15:31 +0800

    1

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,11 @@
 </template>
 
 <script setup lang="ts">
-import debug from '@/utils/debug';
+// import debug from '@/utils/debug';
 
 onMounted(() => {
   // 因为debug是存入localStorage中的，刷新页面会从localStorage取出，根据debug控制是否隐藏
-  debug.init();
+  // debug.init();
   // const { username = '', mobile = '' } = auth.getUser();
 });
 </script>
